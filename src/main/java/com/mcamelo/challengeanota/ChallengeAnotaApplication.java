a4045664fafb2dee3843f540f7f6b4dcfc988840
Mario Camelo <mariuo@gmail.com>
2024-01-15 08:51:28 -0500

    :feat project created

---
+++ src/main/java/com/mcamelo/challengeanota/ChallengeAnotaApplication.java
@@ -0,0 +1,13 @@
+package com.mcamelo.challengeanota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAnotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAnotaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mcamelo/challengeanota/ChallengeAnotaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcamelo.challengeanota;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAnotaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
